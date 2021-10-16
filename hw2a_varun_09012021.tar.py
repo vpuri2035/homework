ab154fd2081594b9c9ece408d504d461bbeffadf
vpuri2035 <89360020+vpuri2035@users.noreply.github.com>
2021-10-15 22:53:26 -0400

    Add files via upload

---
+++ hw2a_varun_09012021.tar.py
@@ -0,0 +1,86 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+get_ipython().set_next_input('How does Pon suggest data might be “securitized” over Ocean Protocol');get_ipython().run_line_magic('pinfo', 'Protocol')
+Ans:To build a tool called Blockchain for providence history of people to
+track their behavior in a way that builds up and not only in
+financially but their intellectual property, their identity and their
+reputation.
+
+
+# In[ ]:
+
+
+2. Who (or what) did Pon say was the “consumer” of Blockchain and big data? How does this relate to “data mining”? 
+Ans Artificial intelligence. Since people are already trying to share data and this data can be mined for insights and industry like AI. 
+Allowing AI user to mini and use financial mean to have signal on prices and giving people back control of their data and tool to monetize as well
+
+
+# In[ ]:
+
+
+Who does Pon say are the gatekeepers of the current “data economy”? How does Ocean Protocol address this? 
+ans: Pon addresses Current Tech media giants like Facebook,Google,amazon etc in USA or Baidu,Alibaba as 
+   gatekeepers of data economy. Ocean datatokens wrap data services as industry-standard ERC20 tokens. 
+   This enables data wallets, data exchanges, and data co-ops by leveraging crypto wallets, exchanges, 
+   and other decentralized finance (DeFi) tools and in parallel working on more powerful tools.
+
+
+# In[ ]:
+
+
+How does Pon suggest data might be “securitized” over Ocean Protocol? Use the example he gives directly. 
+Ans:
+Data is “securitized” over Ocean Protocol by turning data stream into
+value stream.
+These data streams can then be used to speculate the data, for example
+a user has a basket or data stream where he can index data for ex in
+automative sector or/and in  healthcare sector and create investment
+products for himself or others.
+
+
+# In[ ]:
+
+
+What are the four landscape users (or targets) of Ocean Protocol? 
+Developers
+Enterprise
+Individual Users
+Cryptonative users
+
+
+# In[ ]:
+
+
+What example does he give of a liquid dataset that might have tremendous value in a variety of current 
+and future contexts relating to our world? 
+Atmospheric temperature data. We have AT data that people can access and are able to share and keep 
+it updated(specifically remote places), we can get a handle on atmospheric temperature conditions for
+climatic researchers to get a better idea how global warming is affecting us. 
+I like the idea of climate token and contributors can earn a climate token to make data better and some
+incentives to add better data.similar ideas 
+floated for cleaner air ex carbon token, take out carbon out of the air but also bring air.  
+
+
+# In[ ]:
+
+
+What does Pon predict the crypto marketplace will look like in 10 years? 
+n 10 year, we already have couple of 100 billion dollar companies build on top of cyrpto.
+Cypto itself may decentralized but companies like coinbase, finance, data companies,nft companies may worth billions due to impact of global block chaint
+
+
+# In[ ]:
+
+
+After consuming this podcast, please list your reaction to it in a short paragraph of at a minimum of 
+three complete sentences. Address what you already know about crypto and the data economy as well as the 
+things that you learned that were new knowledge. You may also address whether you agree or disagree with Pon’s positions, especially how it relates to Ocean Protocol and cryptocurrencies, in general, becoming a great equalizer. You can also include any personal experiences you have with the technology.
+Ans: Blockchain and crypto are enabling technologies to allow a re-balancing of power. Cypto is a new brand of currency,
+    which will controlled Economy of world and not dictate by few specifc countries.  
+    I agree on most of the point and examples climate token but also contradict that quick expansion 
+    of companies like coinbase in next year and their work increase exponenetly. 
+
